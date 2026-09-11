mahesh-atx/Py_Practice

commit c503900bd698886b070dc73deccdeb674a2354f4

fix(worker): escape backslashes in pyodide python harness template string

js/pyodide-worker.js
@@ -177,7 +177,7 @@ class _SmartStr(str):
                         _pending_tokens.clear()
                     return base + rem
                 else:
-                    combined = str(self) + "\\n" + "\\n".join(pending_extra) + ("\\n" + remaining if remaining else "")
+                    combined = str(self) + "\\\\n" + "\\\\n".join(pending_extra) + ("\\\\n" + remaining if remaining else "")
                     # clear pending as consumed
                     if pending_extra:
                         _pending_tokens.clear()
@@ -208,7 +208,7 @@ def _input(prompt=''):
         xhr.open('GET', f'/api/get-input?id={_req_id}', False)
         xhr.send(None)
         if xhr.status == 200:
-            val = xhr.responseText.rstrip('\n')
+            val = xhr.responseText.rstrip('\\n')
             return _SmartStr(val)
         else:
             raise EOFError('Input cancelled or failed')
@@ -216,9 +216,9 @@ def _input(prompt=''):
         line = _stdin_buf.readline()
         if line == '':
             raise EOFError('No more input values.')
-        if line.endswith('\r\n'):
+        if line.endswith('\\r\\n'):
             stripped = line[:-2]
-        elif line.endswith('\n') or line.endswith('\r'):
+        elif line.endswith('\\n') or line.endswith('\\r'):
             stripped = line[:-1]
         else:
             stripped = line

commit ebdec1903bc65cf4560784b11a8c614aa0dc9b43

fix(worker): correct over-escaped newline in _SmartStr.split joined string
Line 180 had \\\\n (4 backslashes) which cooked to \\n literal in Python instead of \n newline. Revert to \\n (2 backslashes) so JS template cooks to python \n. Fixes semantic bug introduced in c503900 while keeping rstrip/endswith fixes.

js/pyodide-worker.js
@@ -177,7 +177,7 @@ class _SmartStr(str):
                         _pending_tokens.clear()
                     return base + rem
                 else:
-                    combined = str(self) + "\\\\n" + "\\\\n".join(pending_extra) + ("\\\\n" + remaining if remaining else "")
+                    combined = str(self) + "\\n" + "\\n".join(pending_extra) + ("\\n" + remaining if remaining else "")
                     # clear pending as consumed
                     if pending_extra:
                         _pending_tokens.clear()